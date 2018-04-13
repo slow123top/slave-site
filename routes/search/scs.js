d41cf0dcf1819ab200049d9b59de57a093ff81bb
your name <huyuyang_2015@outlook.com>
2018-04-13 15:21:21 +0800

    分布节点-实现星表的锥形检索查询，tgas.dat表格

--- routes/search/scs.js
+++ routes/search/scs.js
@@ -12,22 +12,26 @@ router.post('/', function (req, res, next) {
     let ra = req.body.ra
     let de = req.body.de
     let radius = Number(req.body.radius)
-    let allPath = new path.allPath(platform('1'), 'ac_n30.dat').getPath()
+    // let allPath = new path.allPath(platform('1'), 'ac_n30.dat').getPath()
+    let allPath = new path.allPath(platform('1'), 'tgas.dat').getPath()
     const readLine = readline.createInterface({
         input: fs.createReadStream(allPath)
     });
 
     console.log(req.body)
     readLine.on('line', line => {
-        let raDegree = Number(line.substring(0, 3))
-        let raMinute = Number(line.substring(4, 6))
-        let raSecond = Number(line.substring(7, 13))
-        let raLine = (((raSecond / 60).toFixed(3) + raMinute) / 60).toFixed(3) + raDegree
-        let deDegree = Number(line.substring(14, 17))
-        let deMinute = Number(line.substring(18, 20))
-        let deSecond = Number(line.substring(21, 26))
-        let deLine = (((deSecond / 60).toFixed(3) + deMinute) / 60).toFixed(3) + deDegree
-        let r = Math.acos(Math.sin(deLine) * Math.sin(de) + Math.cos(deLine) * Math.cos(de) * Math.cos(raLine - ra))
+        // let raDegree = Number(line.substring(0, 3))
+        // let raMinute = Number(line.substring(4, 6))
+        // let raSecond = Number(line.substring(7, 13))
+        // let raLine = (((raSecond / 60).toFixed(3) + raMinute) / 60).toFixed(3) + raDegree
+        // let deDegree = Number(line.substring(14, 17))
+        // let deMinute = Number(line.substring(18, 20))
+        // let deSecond = Number(line.substring(21, 26))
+        // let deLine = (((deSecond / 60).toFixed(3) + deMinute) / 60).toFixed(3) + deDegree
+        // let r = Math.acos(Math.sin(deLine) * Math.sin(de) + Math.cos(deLine) * Math.cos(de) * Math.cos(raLine - ra))
+        let lineRadeg = Number(line.substring(75, 89))
+        let lineDedeg = Number(line.substring(98, 111))
+        let r = Math.acos(Math.sin(lineDedeg) * Math.sin(de) + Math.cos(lineDedeg) * Math.cos(ra) * Math.cos(lineRadeg - ra));
         if (Number(r) < radius) {
             // console.log(raLine,deLine,r)
             console.log(line)
